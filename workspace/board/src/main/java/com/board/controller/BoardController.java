7e7dbe94e19b5731e7472dc7d34b72e78c1d1a8e
soonang <nr97819@naver.com>
2021-12-07 11:04:53 +0900

    CRUD delete 구현

--- workspace/board/src/main/java/com/board/controller/BoardController.java
+++ workspace/board/src/main/java/com/board/controller/BoardController.java
@@ -23,7 +23,6 @@ public class BoardController {
 	// 게시물 목록
 	@RequestMapping(value="/list", method=RequestMethod.GET)
 	public void getList(Model model) throws Exception {
-		
 		List<BoardVO> list = service.list();
 		model.addAttribute("list", list);
 	}
@@ -38,14 +37,12 @@ public class BoardController {
 	@RequestMapping(value="/write", method=RequestMethod.POST)
 	public String postWrite(BoardVO vo) throws Exception {
 		service.write(vo);
-		
 		return "redirect:/board/list";
 	}
 	
 	// 게시물 조회
 	@RequestMapping(value = "/view", method = RequestMethod.GET)
 	public void getView(@RequestParam("bno") int bno, Model model) throws Exception {
-		
 		BoardVO vo = service.view(bno);
 		model.addAttribute("view", vo);
 	}
@@ -53,7 +50,6 @@ public class BoardController {
 	// 게시물 수정
 	@RequestMapping(value = "/modify", method = RequestMethod.GET)
 	public void getModify(@RequestParam("bno") int bno, Model model) throws Exception {
-		
 		BoardVO vo = service.view(bno);
 		model.addAttribute("view", vo);
 	}
@@ -61,13 +57,16 @@ public class BoardController {
 	// 게시물 수정
 	@RequestMapping(value = "/modify", method = RequestMethod.POST)
 	public String postModify(BoardVO vo) throws Exception {
-			
 		service.modify(vo);
 		return "redirect:/board/view?bno=" + vo.getBno();
 	}
 
-
-		
+	// 게시물 삭제
+	@RequestMapping(value = "/delete", method = RequestMethod.GET)
+	public String getDelete(@RequestParam("bno") int bno) throws Exception {
+		service.delete(bno);
+		return "redirect:/board/list";
+	}
 		
 }
 
--- workspace/board/src/main/java/com/board/dao/BoardDAOImpl.java
+++ workspace/board/src/main/java/com/board/dao/BoardDAOImpl.java
@@ -20,29 +20,31 @@ public class BoardDAOImpl implements BoardDAO {
 	// 게시물 목록
 	@Override
 	public List list() throws Exception {
-		
 		return sql.selectList(namespace + ".list");
 	}
 
 	// 게시물 작성
 	@Override
 	public void write(BoardVO vo) throws Exception {
-		
 		sql.insert(namespace + ".write", vo);
 	}
 
 	// 게시물 조회
 	@Override
 	public BoardVO view(int bno) throws Exception {
-		
 		return sql.selectOne(namespace + ".view", bno);
 	}
 	
 	// 게시물 수정
 	@Override
 	public void modify(BoardVO vo) throws Exception {
-		
 		sql.update(namespace + ".modify", vo);
+	}
+
+	// 게시물 삭제
+	@Override
+	public void delete(int bno) throws Exception {
+		sql.delete(namespace + ".delete", bno);
 	}
 
 }
